0215f631e781636eb674412037b2b71d253d8e76
faulty337 <faulty337@gmail.com>
2023-02-21 10:59:25 +0900

    Feat : 프로젝트 개설(#1)

---
+++ src/main/java/com/instantduo/codeswitching/CodeSwitchingApplication.java
@@ -0,0 +1,13 @@
+package com.instantduo.codeswitching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSwitchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeSwitchingApplication.class, args);
+	}
+
+}
