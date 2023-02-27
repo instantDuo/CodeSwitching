ee784507e206b6f2eedadff35f679fcbe8a3ad61
faulty337 <faulty337@gmail.com>
2023-02-27 16:44:30 +0900

    Feat : Jwt 로직 추가(#4)

--- src/main/java/com/instantduo/codeswitching/user/UserController.java
+++ src/main/java/com/instantduo/codeswitching/user/UserController.java
@@ -5,6 +5,8 @@ import com.instantduo.codeswitching.common.ResponseMessage;
 import com.instantduo.codeswitching.dto.request.LoginRequest;
 import com.instantduo.codeswitching.dto.request.SignupRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.util.Pair;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,19 +25,24 @@ import javax.validation.Valid;
 public class UserController {
     private final UserService userService;
 
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
 
     @PostMapping("/signup")
-    public ResponseEntity signup(@RequestBody @Valid SignupRequest signupRequest){
+    public ResponseEntity<ResponseMessage<String>> signup(@RequestBody @Valid SignupRequest signupRequest){
         userService.signup(signupRequest);
-        ResponseMessage message = new ResponseMessage("회원가입이 완료되었습니다.", 200);
-        return new ResponseEntity(message, HttpStatus.valueOf(message.getStatusCode()));
+        ResponseMessage<String> message = new ResponseMessage<>("회원가입이 완료되었습니다.", 200);
+        return new ResponseEntity<>(message, HttpStatus.valueOf(message.getStatusCode()));
     }
 
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody @Valid LoginRequest loginRequest){
-        userService.login(loginRequest);
-        ResponseMessage message = new ResponseMessage("로그인이 완료되엇습니다.", 200);
-        return new ResponseEntity(message, HttpStatus.valueOf(message.getStatusCode()));
+    public ResponseEntity<ResponseMessage<String>> login(@RequestBody @Valid LoginRequest loginRequest, HttpServletResponse response){
+        Pair<String, String> token = userService.login(loginRequest);
+        ResponseMessage<String> message = new ResponseMessage<>("로그인이 완료되엇습니다.", 200);
+
+        response.addHeader(token.getFirst(), token.getSecond());
+
+        return new ResponseEntity<>(message, HttpStatus.valueOf(message.getStatusCode()));
 
     }
 
--- src/main/java/com/instantduo/codeswitching/user/UserService.java
+++ src/main/java/com/instantduo/codeswitching/user/UserService.java
@@ -2,11 +2,16 @@ package com.instantduo.codeswitching.user;
 
 import com.instantduo.codeswitching.common.exception.CustomException;
 import com.instantduo.codeswitching.common.exception.ErrorCode;
+import com.instantduo.codeswitching.common.jwt.JwtUtil;
 import com.instantduo.codeswitching.dto.request.LoginRequest;
 import com.instantduo.codeswitching.dto.request.SignupRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.util.Pair;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +20,8 @@ public class UserService {
     private final UserRepository userRepository;
 
     private final PasswordEncoder passwordEncoder;
+
+    private final JwtUtil jwtUtil;
 
     public void signup(SignupRequest signupRequest) {
         if(userRepository.existsByLoginId(signupRequest.getLoginId())){
@@ -34,7 +41,7 @@ public class UserService {
         userRepository.save(user);
     }
 
-    public void login(LoginRequest loginRequest) {
+    public Pair<String, String> login(LoginRequest loginRequest) {
         User user = userRepository.findByLoginId(loginRequest.getLoginId()).orElseThrow(
                 () -> new CustomException(ErrorCode.NOT_FOUND_USER)
         );
@@ -43,5 +50,8 @@ public class UserService {
             throw new CustomException(ErrorCode.NOT_FOUND_USER);
         }
 
+        String token = jwtUtil.createToken(user.getLoginId());
+
+        return Pair.of(JwtUtil.AUTHORIZATION_HEADER, token);
     }
 }
